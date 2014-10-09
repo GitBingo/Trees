f7ecdb574c9385fc93295da5218a11ce3525c57b
bryonglass@gmail.com <bryonglass@blackhillsinfosec.com>
2014-10-09 13:47:20 -0600

    fixed the include file to #include /linklist.h/

--- linkList/linklist.cpp
+++ linkList/linklist.cpp
@@ -1,6 +1,18 @@
-#include <linklist.h>
+#include "linklist.h"
 
 LinkList::LinkList()
 {
   headptr = NULL;
+}
+
+LinkList::~LinkList()
+{
+  node *temp;
+  while( headptr != NULL )
+  {
+    temp = headptr;
+    headptr = headptr -> next;
+    delete temp;
+  }
+  
 }
--- linkList/linklist.h
+++ linkList/linklist.h
@@ -1,5 +1,6 @@
 
 
+
 #include <cmath>
 #include <iomanip>
 #include <fstream>
